d2f61b83462f7802a270bd38f6a57670357945be
Harish Babu <harish.babu@codurance.com>
2020-07-10 17:47:58 +0100

    project setup with SqueakesRepository

---
+++ test/Squeaker.UnitTests/SqueakesRepositoryTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Squeaker.Application;
+using Xunit;
+
+namespace Squeaker.UnitTests
+{
+    public class SqueakesRepositoryTests
+    {
+        [Fact]
+        public void Nothing()
+        {
+            var sut = new SqueakesRepository();
+        }
+    }
+}
